b5d032e27a9ff9ffbecd5edd6c81d75f6ef45cc9
5237-mests <99667573+5237-mests@users.noreply.github.com>
2022-12-21 01:08:42 +0300

    promise

--- 0x00-ES6_basic/1-main.js
+++ 0x00-ES6_basic/1-main.js
@@ -2,3 +2,13 @@ import taskBlock from './1-block-scoped.js';
 
 console.log(taskBlock(true));
 console.log(taskBlock(false));
+// var a = 10;
+// {
+//     var a = -10;
+// }
+// let b = a;
+// {
+//     let b = -20;
+// }
+
+// console.log(b)
--- 0x00-pascal_triangle/README.md
+++
@@ -1,1 +0,0 @@
-## Paskal triangle
