18ea540dc185925276b4344330a92fbee89ce9fa
5237-mests <99667573+5237-mests@users.noreply.github.com>
2022-12-21 23:49:05 +0300

    rej

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,3 +1,9 @@
 export default function uploadPhoto(filename) {
-  return Promise.reject(`Error: ${filename} cannot be processed`)
+  return new Promise((resolve, reject) => {
+    if (filename) {
+      reject(new Error(`${filename} cannot be processed`));
+    } else {
+      resolve(true);
+    }
+  });
 }
