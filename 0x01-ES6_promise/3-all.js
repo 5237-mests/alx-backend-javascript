9e21e93fd2a9d89b9ba308bb0aab36983691c913
5237-mests <99667573+5237-mests@users.noreply.github.com>
2022-12-21 14:19:22 +0300

    eslint

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,10 +1,10 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const res2 = uploadPhoto();
-  const res1 = createUser();
+  const res1 = uploadPhoto();
+  const res2 = createUser();
 
   Promise.all([res1, res2])
-    .then((data) => console.log(data[1].body, data[0].firstName, data[0].lastName))
+    .then((data) => console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`))
     .catch(() => console.log('Signup system offline'));
 }
