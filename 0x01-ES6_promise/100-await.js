d7b24b1143fa1785fbd5b8ff01b7bb09c42fa57a
5237-mests <99667573+5237-mests@users.noreply.github.com>
2022-12-21 23:01:02 +0300

    fail

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -4,8 +4,14 @@ export default async function asyncUploadUser() {
   const res1 = await uploadPhoto();
   const res2 = await createUser();
 
-  return {
-    photo: res1,
-    user: res2,
-  };
+  const obj = {};
+
+  if (!res1 || !res2) {
+    obj.photo = null;
+    obj.user = null;
+  } else {
+    obj.photo = res1;
+    obj.user = res2;
+  }
+  return obj;
 }
