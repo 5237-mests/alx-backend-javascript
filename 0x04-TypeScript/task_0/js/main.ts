98d78f2d8a0a3074450957d67883a2cd5a1d1882
5237-mests <99667573+5237-mests@users.noreply.github.com>
2023-01-04 16:14:02 +0300

    f

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,22 @@
+interface Student {
+  firstName: string,
+  lastName: string,
+  age: number,
+  location: string
+}
+
+let student1: Student = {
+  firstName: 'Mesfin',
+  lastName: 'Mul',
+  age: 28,
+  location:'Ethiopia'
+}
+
+let student2: Student = {
+  firstName: 'Mes',
+  lastName: 'Mulu',
+  age: 23,
+  location:'Ethiopia'
+}
+
+let studentsList = [student1, student2];
