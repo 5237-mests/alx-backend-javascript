207f0be45677f68e2ffbbc507f416095bf4a7d94
5237-mests <msfnmulgeta@gmail.com>
2023-02-20 21:59:11 +0300

    m

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,5 +1,5 @@
-#!/usr/bin/node
 process.stdin.setEncoding('utf8');
+
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('readable', function () {
   const inputName = process.stdin.read();
