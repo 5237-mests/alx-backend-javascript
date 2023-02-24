7520a20a1fefdf953b790c3798d2bf2e77f32de4
5237-mests <msfnmulgeta@gmail.com>
2023-02-24 23:49:46 +0300

    stub

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,20 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+const sendPaymentRequestToApi = require('./3-payment');
+const Utils = require('./utils');
+
+describe('sendPaymentRequestToApi function', () => {
+  it('sendPaymentRequestToApi verify that console.log is logging the correct message The total is: 10', () => {
+    const bigBrother = sinon.spy(console);
+    const dummy = sinon.stub(Utils, 'calculateNumber');
+
+    dummy.returns(10);
+    sendPaymentRequestToApi(100, 20);
+    expect(dummy.calledWith('SUM', 100, 20)).to.be.true;
+    expect(dummy.callCount).to.be.equal(1);
+    expect(bigBrother.log.calledWith('The total is: 10')).to.be.true;
+    expect(bigBrother.log.callCount).to.be.equal(1);
+    dummy.restore();
+    bigBrother.log.restore();
+  });
+});
