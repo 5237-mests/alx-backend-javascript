35df9b8ec949ed164cafedcffe31ea8af9075445
5237-mests <msfnmulgeta@gmail.com>
2023-02-25 09:13:59 +0300

    fixed

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -42,41 +42,41 @@ describe('calculateNumber()', function () {
     });
   });
   
-  describe('type === SUBSTRACT', () => {
+  describe('type === SUBTRACT', () => {
     it('sum 2', function () {
-      expect(calculateNumber('SUBSTRACT', -1.4, -4.5)).to.equal(3);
+      expect(calculateNumber('SUBTRACT', -1.4, -4.5)).to.equal(3);
     });
 
-    it('floating point whole numbers (SUBSTRACT)', () => {
-      expect(calculateNumber('SUBSTRACT', 1.0, 2.0)).to.equal(-1);
+    it('floating point whole numbers (SUBTRACT)', () => {
+      expect(calculateNumber('SUBTRACT', 1.0, 2.0)).to.equal(-1);
     });
 
-    it('rounding down b\'s floating point fractional number(SUBSTRACT)', () => {
-      expect(calculateNumber('SUBSTRACT',1.0, 2.4)).to.equal(-1);
+    it('rounding down b\'s floating point fractional number(SUBTRACT)', () => {
+      expect(calculateNumber('SUBTRACT',1.0, 2.4)).to.equal(-1);
     });
 
-    it('rounding down a and b\'s floating point fractional number(SUBSTRACT)', () => {
-      expect(calculateNumber('SUBSTRACT', 1.4, 2.4)).to.equal(-1);
+    it('rounding down a and b\'s floating point fractional number(SUBTRACT)', () => {
+      expect(calculateNumber('SUBTRACT', 1.4, 2.4)).to.equal(-1);
     });
 
-    it('rounding down a\'s floating point fractional number(SUBSTRACT)', () => {
-      expect(calculateNumber('SUBSTRACT', 1.4, 2.0)).to.equal(-1);
+    it('rounding down a\'s floating point fractional number(SUBTRACT)', () => {
+      expect(calculateNumber('SUBTRACT', 1.4, 2.0)).to.equal(-1);
     });
 
-    it('rounding up b\'s floating point fractional numbers(SUBSTRACT)', () => {
-      expect(calculateNumber('SUBSTRACT', 1.0, 2.5)).to.equal(-2);
+    it('rounding up b\'s floating point fractional numbers(SUBTRACT)', () => {
+      expect(calculateNumber('SUBTRACT', 1.0, 2.5)).to.equal(-2);
     });
 
-    it('rounding up a and b\'s floating point fractional numbers(SUBSTRACT)', () => {
-      expect(calculateNumber('SUBSTRACT', 2.6, 2.5)).to.equal(0);
+    it('rounding up a and b\'s floating point fractional numbers(SUBTRACT)', () => {
+      expect(calculateNumber('SUBTRACT', 2.6, 2.5)).to.equal(0);
     });
 
-    it('rounding up a\'s floating point fractional numbers(SUBSTRACT)', () => {
-      expect(calculateNumber('SUBSTRACT', 2.6, 2.0)).to.equal(1);
+    it('rounding up a\'s floating point fractional numbers(SUBTRACT)', () => {
+      expect(calculateNumber('SUBTRACT', 2.6, 2.0)).to.equal(1);
     });
 
-    it('rounding down a and b floating point fractional numbers with trailing 9\'s (SUBSTRACT)', () => {
-      expect(calculateNumber('SUBSTRACT', 2.499999, 3.499999)).to.equal(-1);
+    it('rounding down a and b floating point fractional numbers with trailing 9\'s (SUBTRACT)', () => {
+      expect(calculateNumber('SUBTRACT', 2.499999, 3.499999)).to.equal(-1);
     });
   });
   
